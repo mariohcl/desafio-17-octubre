86c8a291c8671d080a25c0d212e05ea75fbfced7
mario <air.hammer@gmail.com>
2019-10-24 16:45:38 -0300

    agregado css al proyecto en todas sus vistas

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -49,7 +49,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.html { redirect_to @product, notice: 'El producto fue creado' }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
@@ -63,7 +63,7 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.html { redirect_to @product, notice: 'El producto fue actualizado' }
         format.json { render :show, status: :ok, location: @product }
       else
         format.html { render :edit }
@@ -77,7 +77,7 @@ class ProductsController < ApplicationController
   def destroy
     @product.destroy
     respond_to do |format|
-      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+      format.html { redirect_to products_url, notice: 'El producto fue eliminado.' }
       format.json { head :no_content }
     end
   end
